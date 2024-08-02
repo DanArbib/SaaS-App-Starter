5f2bbcd1e41b52174c0c842bf13b0e2049b1e3df
Glivs <support@glivs.com>
2024-08-02 22:18:01 +0300

    build(app): main app build

---
+++ app/src/components/Site/Contact.vue
@@ -0,0 +1,114 @@
+<template>
+    <!-- ====== Contact Start ====== -->
+    <section id="contact" class="relative py-20 md:py-[120px]">
+      <div class="container px-4">
+        <div class="mx-auto mb-[60px] max-w-[520px] text-center">
+          <span class="mb-2 block text-lg font-semibold text-primary">
+            Contact
+          </span>
+        </div>
+        <div class="-mx-4 flex flex-wrap items-center">
+          
+          <div class="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div class="ud-contact-content-wrapper">
+              <div class="ud-contact-title mb-12 lg:mb-[150px]">
+              <h2
+                class="text-left mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-[40px]"
+              >
+              Contact us
+              </h2>
+              <p class="max-w-[485px] text-base text-body-color dark:text-dark-6">
+               fsdfsfsfsfdsfd
+              </p>
+
+              </div>
+            </div>
+          </div>
+          <div class="w-full px-4 lg:w-5/12 xl:w-4/12">
+            <div
+              class="wow fadeInUp rounded-lg bg-white px-8 py-10 shadow-testimonial dark:bg-dark-2 dark:shadow-none sm:px-10 sm:py-12 md:p-[60px] lg:p-10 lg:px-10 lg:py-12 2xl:p-[60px]"
+              data-wow-delay=".2s
+              "
+            >
+              <h3
+                class="mb-8 text-2xl font-semibold text-dark dark:text-white md:text-[28px] md:leading-[1.42]"
+              >
+                Send us a Message
+              </h3>
+              <form>
+                <div class="mb-[22px]">
+                  <label
+                    for="fullName"
+                    class="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    >Full Name</label
+                  >
+                  <input
+                    type="text"
+                    name="fullName"
+                    placeholder=""
+                    class="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-body-color placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-dark-6"
+                  />
+                </div>
+                <div class="mb-[22px]">
+                  <label
+                    for="email"
+                    class="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    >Email</label
+                  >
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder=""
+                    class="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-body-color placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-dark-6"
+                  />
+                </div>
+                <div class="mb-[22px]">
+                  <label
+                    for="phone"
+                    class="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    >Phone</label
+                  >
+                  <input
+                    type="text"
+                    name="phone"
+                    placeholder=""
+                    class="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-body-color placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-dark-6"
+                  />
+                </div>
+                <div class="mb-[30px]">
+                  <label
+                    for="message"
+                    class="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    >Message</label
+                  >
+                  <textarea
+                    name="message"
+                    rows="1"
+                    placeholder=""
+                    class="w-full resize-none border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-body-color placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-dark-6"
+                  ></textarea>
+                </div>
+                <div class="mb-0">
+                  <button
+                    type="submit"
+                    class="inline-flex items-center justify-center rounded-md bg-primary px-10 py-3 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-blue-dark"
+                  >
+                    Send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- ====== Contact End ====== -->
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- app/src/views/Dashboard/Main.vue
+++ app/src/views/Dashboard/Main.vue
@@ -1,21 +1,13 @@
-<template>
-  <div>
-
-  </div>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  setup () {
-    
-
-    return {}
-  }
-})
+<script setup lang="ts">
+import SiteLayout from '@/layouts/DashboardLayout.vue'
+import { useAuthStore } from '@/store/auth';
+const authStore = useAuthStore();
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <SiteLayout>
+    <div class="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+      {{ authStore.userData }}
+    </div>
+  </SiteLayout>
+</template>
