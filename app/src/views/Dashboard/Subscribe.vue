db2c9cd6b1d12a294e5744a5f7a5b0e45c8ee5a3
Glivs <support@glivs.com>
2024-08-02 22:28:07 +0300

    fix(app): remove base from subscribe page

--- app/src/views/Dashboard/Subscribe.vue
+++ app/src/views/Dashboard/Subscribe.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app" class="h-full">
-    <Board />
+  <div>
+
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Board from '@/components/Board.vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'App',
-  components: { Board },
-});
+  setup () {
+    
+
+    return {}
+  }
+})
 </script>
 
-<style>
-/* Global styles */
-html, body, #app {
-  height: 100%;
-  margin: 0;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-}
+<style scoped>
 
-.smooth-dnd-container.horizontal {
-  display: flex !important;
-}
 </style>
